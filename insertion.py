eaf618b59ea3073883a2cedb4d27510c189fa142
Nilabh Sahu <59756708+nilabh22@users.noreply.github.com>
2021-09-26 20:47:41 +0530

    Add files via upload

---
+++ insertion.py
@@ -0,0 +1,26 @@
+import time
+import random
+
+start = time.time()
+
+def insertionsort(A):
+    #we start loop at second element (index 1) since the first item is already sorted
+    for j in range(1,len(A)):
+        key = A[j] #The next item we are going to insert into the sorted section of the array
+
+        i = j-1 #the last item we are going to compare to
+        #now we keep moving the key back as long as it is smaller than the last item in the array
+        while (i > -1) and key < A[i]: #if i == -1 means that this key belongs at the start
+            A[i+1]=A[i] #move the last object compared one step ahead to make room for key
+            i=i-1 #observe the next item for next time.
+        #okay i is not greater than key means key belongs at i+1
+        A[i+1] = key
+    return A
+list_data = list(range(1,1001))
+random.shuffle(list_data)
+print(list_data)
+
+end = time.time()
+print(insertionsort(list_data))
+runtime = (end-start)
+print("Runtime for Insertion Sort: ",runtime)
